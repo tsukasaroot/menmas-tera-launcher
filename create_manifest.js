a2a21a775ae8e92271ec11e69b35e1fd5b79a947
Jordan <jordan.rouco-miguez@epitech.eu>
2022-06-12 13:58:47 +0200

    update to 1.4.3

---
+++ create_manifest.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const { join } = require('path');
+
+const inputPath = join(__dirname, 'release/MenmasTeraInstaller.zip');
+const outputPath = join(__dirname, 'release/manifest.json');
+const versionPath = join(__dirname, 'version.txt');
+
+let file = fs.readFileSync(inputPath);
+let hash = crypto.createHash('sha256').update(file).digest('hex');
+let version = fs.readFileSync(versionPath, 'utf-8');
+
+fs.writeFileSync(outputPath, JSON.stringify({
+    version,
+    path: "MenmasTeraInstaller.zip",
+    hash
+}, null, 4));
